fac99edd49ee72b4fe6e812d6ce634d6bb8e1b19
Spencer Smith <stix1919@hotmail.com>
2019-04-04 10:01:10 -0600

    Did session demo

--- server/index.js
+++ server/index.js
@@ -1,10 +1,33 @@
 require('dotenv').config()
 const express = require('express')
+const session = require('express-session')
 
 const app = express()
-const {PORT_NUM} = process.env
+const {PORT_NUM, SESSION_SECRET} = process.env
 
 app.use(express.json())
+app.use(session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 10
+    }
+}))
+
+app.post('/auth/login', (req, res) => {
+    const {username, password} = req.body
+    req.session.user = {username, password}
+    res.status(200).send(req.session)
+})
+app.get('/auth/getUser', (req, res) => {
+    if(req.session.user){
+        res.status(200).send(req.session.user)
+    } else {
+        res.status(401).send('log in plz')
+    }
+
+})
 
 
 app.listen(PORT_NUM, () => {
--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
 import React, { Component } from 'react';
+
+import axios from 'axios'
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      username: '',
+      password: '',
+      loggedIn: false
+    }
+  }
+
+  componentDidMount() {
+    axios.get('/auth/getUser').then(response => {
+      const {username, password} = response.data
+      this.setState({username, password, loggedIn: true})
+      console.log(response.data, 'in cdm')
+    })
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  loginUser = () => {
+    const {password, username} = this.state
+    axios.post('/auth/login', {password, username}).then(response => {
+      this.setState({loggedIn: true})
+      console.log(response.data, 'in cdm')
+    })
+  }
+
+
   render() {
+    console.log(this.state, 'message')
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          {this.state.loggedIn 
+            ? <h1>Thanks for loggin in: {this.state.username}</h1>
+            : <div>
+              <input onChange={this.handleChange} value={this.state.username} name='username'/>
+              <input onChange={this.handleChange} value={this.state.password} name='password'/>
+              <button onClick={this.loginUser}>Sign in</button>
+            </div>
+          }
+          
+          
         </header>
       </div>
     );
