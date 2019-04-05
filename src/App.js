6e6177bece2ce244fe756bdbba02ba4151d6aeaf
Spencer Smith <stix1919@hotmail.com>
2019-04-05 10:58:30 -0600

    cleaning up in prep for actual project work

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 
-import Button from './components/hookDemo'
-
 import axios from 'axios'
 import logo from './logo.svg';
 import './App.css';
@@ -10,51 +8,15 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      username: '',
-      password: '',
-      loggedIn: false
     }
-  }
-
-  componentDidMount() {
-    axios.get('/auth/getUser').then(response => {
-      const {username, password} = response.data
-      this.setState({username, password, loggedIn: true})
-      console.log(response.data, 'in cdm')
-    })
-  }
-
-  handleChange = (event) => {
-    this.setState({
-      [event.target.name]: event.target.value
-    })
-  }
-
-  loginUser = () => {
-    const {password, username} = this.state
-    axios.post('/auth/login', {password, username}).then(response => {
-      this.setState({loggedIn: true})
-      console.log(response.data, 'in cdm')
-    })
   }
 
 
   render() {
-    console.log(this.state, 'message')
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <Button />
-          {this.state.loggedIn 
-            ? <h1>Thanks for loggin in: {this.state.username}</h1>
-            : <div>
-              <input onChange={this.handleChange} value={this.state.username} name='username'/>
-              <input onChange={this.handleChange} value={this.state.password} name='password'/>
-              <button onClick={this.loginUser}>Sign in</button>
-            </div>
-          }
-          
           
         </header>
       </div>
--- src/components/hookDemo.js
+++ src/components/hookDemo.js
@@ -4,17 +4,7 @@ import React, { useState } from 'react'
 
 export default function Button() {
     const [buttonText, setButtonText] = useState("Click me, please")
-
-
-
-
-
-
-
-
-
     
-
     return (
         <button onClick={() => setButtonText("Thanks, been clicked!")}>
             {buttonText}
