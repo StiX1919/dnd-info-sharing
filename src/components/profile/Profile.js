2ab33e37d349930e3e0172dea3e6309772c4d9d8
Spencer Smith <stix1919@hotmail.com>
2019-04-11 12:22:08 -0600

    cool input css

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -11,15 +11,18 @@ class Profile extends Component {
         return (
             <div className={profile ? 'profile-bar hidden' : 'profile-bar'}>
                 <h2>Profile:</h2>
-                <h3>Username:</h3>
-                <input placeholder='Username' value={user.username}/>
-                <h3>Profile Picture:</h3>
-                <input placeholder='Username' value={user.user_image}/>
+                <input type='text' id='username-input' className='prof-input name-text' placeholder={`UserName: ${user.username}`} />
+                <label htmlFor="username-input" className="username-label">Username</label>
+                <input type='text' id='prof-pic-input' className='prof-input pic-text' placeholder='New Profile image'/>
+                <label htmlFor="prof-pic-input" className="prof-pic-label">Profile Picture</label>
+                <img className='current-pic' src={user.user_image} alt='current profile pic'/>
                 <button>Logout</button>
             </div>
         )
     }
 }
+
+// maybe bring in s3 or something to import pics from computers
 
 const mapStateToProps = state => {return {...state.userReducer}}
 
